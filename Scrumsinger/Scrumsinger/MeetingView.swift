45ccf4f6933180f7151e4e7fd19e58f64074aecd
avvie <pandora.konof@gmail.combrew>
2026-06-01 09:47:19 +0300

    Creating the first base for view in swift

---
+++ Scrumsinger/Scrumsinger/MeetingView.swift
@@ -0,0 +1,24 @@
+import SwiftUI
+
+struct MeetingView: View {
+    var body: some View {
+        VStack {
+            ProgressView(value: 5, total: 15)
+            HStack{
+                VStack{
+                    Text("Seconds Elapsed:")
+                    Label("300", systemImage: "hourglass.tophalf.fill")
+                }
+                VStack{
+                    Text("Seconds Remaining:")
+                    Label("600", systemImage: "hourglass.bottomhalf.fill")
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    MeetingView()
+}
+
---
+++ Scrumsinger/Scrumsinger/ScrumsingerApp.swift
@@ -0,0 +1,10 @@
+import SwiftUI
+
+@main
+struct ScrumsingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MeetingView()
+        }
+    }
+}
